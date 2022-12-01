1ed5abf38edac81410862ed17ac31a5f25425e03
Kinga Tos <toskinga@interia.pl>
2022-12-01 19:57:41 +0100

    task list without interactions yet

---
+++ src/Buttons/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+const Buttons = ({tasks, hiddenDoneTasks}) => (
+  <div className="buttons">
+        {tasks.length > 0 && (
+          <>
+            <button className="buttons__button ">
+              {hiddenDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+            </button>
+            <button 
+              className="buttons__button"
+              disabled={tasks.every(({ done }) => done)}
+            >
+              Ukończ wszystkie
+            </button>
+          </>
+       )}
+    </div>
+);
+
+
+export default Buttons;
---
+++ src/Tasks/index.js
@@ -0,0 +1,23 @@
+import "./style.css";
+
+const Tasks = ({ tasks, hiddenDoneTasks }) => (
+    <ul className="tasks">
+        {tasks.map(task => (
+            <li
+                className={`list__item${task.done && hiddenDoneTasks ? " list__item--hidden" : ""}`}
+                key={task.id}
+              >
+                 <button className="button__list">
+                    {task.done ? "✔" : ""}
+                </button>
+                <p className={`list__form${task.done ? " list__itemDone" : ""}`}>
+                    {task.content}
+                </p>
+                <button className="button__listRemove">
+                </button>
+              </li>
+        ))}
+    </ul>
+);
+
+export default Tasks;
