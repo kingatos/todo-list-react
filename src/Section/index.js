a59ead7b34ede928941e38659bcdfdaa2d956724
Kinga Tos <toskinga@interia.pl>
2022-12-03 17:55:56 +0100

    formatted code

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,7 +2,8 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
-        <h2 className="section__header">{title}
+        <h2 className="section__header">
+            {title}
             {extraHeaderContent}
         </h2>
         <div className="section__body">
