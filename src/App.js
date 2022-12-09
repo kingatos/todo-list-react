0bd950fa185b2af9db30bde1fb5d284a7cfd1668
Kinga Tos <toskinga@interia.pl>
2022-12-09 19:11:14 +0100

    toggleTaskDone interaction

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header
@@ -35,10 +45,11 @@ function App() {
       <Section
         title="Lista zadań"
         body={
-          <Tasks 
-          tasks={tasks} 
-          hiddenDone={hiddenDone}
-          removeTask={removeTask}
+          <Tasks
+            tasks={tasks}
+            hiddenDone={hiddenDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
           />
         }
         extraHeaderContent={
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,19 +1,23 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hiddenDone, removeTask }) => (
+const Tasks = ({ tasks, hiddenDone, removeTask, toggleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li
                 className={`list__item${task.done && hiddenDone ? " list__item--hidden" : ""}`}
                 key={task.id}
             >
-                <button className="button__list">
+                <button
+                    onClick={() => toggleTaskDone(task.id)}
+                    className="button__list">
                     {task.done ? "✔" : ""}
                 </button>
                 <p className={`list__form${task.done ? " list__itemDone" : ""}`}>
                     {task.content}
                 </p>
-                <button onClick={() => removeTask(task.id)} className="button__listRemove">
+                <button
+                    onClick={() => removeTask(task.id)}
+                    className="button__listRemove">
                 </button>
             </li>
         ))}
