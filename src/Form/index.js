153f478f9530a395c18f6250a896b9a0357d38be
Kinga Tos <toskinga@interia.pl>
2022-12-10 15:06:41 +0100

    added the focus onthe button click

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import "./style.css";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+
+        if (inputRef.current.value.trim() === ""); {
+            inputRef.current.focus();
+        } return;
     };
 
     return (
@@ -15,11 +20,15 @@ const Form = ({ addNewTask }) => {
             className="form"
             onSubmit={onFormSubmit}>
             <input value={newTaskContent}
+                ref={inputRef}
                 className="form__input"
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
             />
-            <button className="form__button">Dodaj zadanie</button>
+            <button
+                className="form__button"
+            >
+                Dodaj zadanie</button>
         </form>
     )
 };
