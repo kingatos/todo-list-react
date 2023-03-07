b590326dd8aa6fb5bd1010f5bad02050eddc28b0
Kinga Tos <toskinga@interia.pl>
2023-03-07 20:30:21 +0100

    added Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 import { GlobalStyles } from "./GlobalStyles";
 import { theme } from "./theme";
 import App from "./App";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
     <GlobalStyles />
     <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
