17cee4b1d8e64e59acdb61ef391cfc7614730638
Kinga Tos <toskinga@interia.pl>
2023-03-08 21:22:21 +0100

    deleted syntax from custom hook

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import Form from "./Form";
 import TasksList from "./TasksList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
-import useTasks from "../../useTasks";
 import styled from "styled-components";
 
 export const StyledContainer = styled.main`
@@ -14,21 +12,6 @@ export const StyledContainer = styled.main`
 `;
 
 function Tasks() {
-  const [hiddenDone, setHiddenDone] = useState(false);
-
-  const toggleHiddenDone = () => {
-    setHiddenDone(hiddenDone => !hiddenDone);
-  };
-
-  const {
-    tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask
-  } = useTasks();
-
-
   return (
     <StyledContainer>
       <Header
@@ -36,30 +19,12 @@ function Tasks() {
       />
       <Section
         title="Dodaj nowe zadanie"
-        body={
-          <Form
-            addNewTask={addNewTask}
-          />
-        }
+        body={<Form />}
       />
       <Section
         title="Lista zadań"
-        body={
-          <TasksList
-            tasks={tasks}
-            hiddenDone={hiddenDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hiddenDone={hiddenDone}
-            toggleHiddenDone={toggleHiddenDone}
-            setAllDone={setAllDone}
-          />
-        }
+        body={<TasksList />}
+        extraHeaderContent={<Buttons />}
       />
     </StyledContainer>
   );
