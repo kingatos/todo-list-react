2e9ed96a77aafad5ba9638f00104eddb9ff34f2d
Kinga Tos <toskinga@interia.pl>
2023-03-13 20:36:15 +0100

    payload define

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -13,12 +13,12 @@ const tasksSlice = createSlice({
     toggleHideDone: (state) => {
       state.hideDone = !state.hideDone;
     },
-    toggleTaskDone: ({ tasks }, { payload: task }) => {
-      const index = tasks.findIndex(({ id }) => id === task);
+    toggleTaskDone: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex(({ id }) => id === taskId);
       tasks[index].done = !tasks[index].done;
     },
-    removeTasks: ({ tasks }, { payload: task }) => {
-      const index = tasks.findIndex(({ id }) => id === task);
+    removeTasks: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex(({ id }) => id === taskId);
       tasks.splice(index, 1);
     },
     setAllDone: (state) => {
