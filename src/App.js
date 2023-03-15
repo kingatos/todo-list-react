3e4372576ea40deb0f187da17b4dad97b46ddee2
Kinga Tos <toskinga@interia.pl>
2023-03-15 20:05:15 +0100

    added the HashRouter and Route, Switch to my App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { HashRouter, Switch, Link, Route } from "react-router-dom";
+import Author from "./features/author/Author";
 import Tasks from "./features/tasks/Tasks";
 
-const App = () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+        <Route path="/autor">
+          <Author />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
 
 export default App;
