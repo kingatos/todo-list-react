af95a6addbd544665c16b3d3ab3ab46f4c28a4a1
Kinga Tos <toskinga@interia.pl>
2023-03-16 20:47:31 +0100

    created new page to single task

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,32 @@
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTasksById } from "../tasksSlice";
+
+export const StyledContainer = styled.main`
+  max-width: 900px;
+  padding: 20px;
+  margin: 0 auto;
+`;
+
+function TaskPage() {
+  const { id } = useParams();
+  const task = useSelector((state) => getTasksById(state, id));
+  return (
+    <StyledContainer>
+      <Header title="Szczegóły zadania" />
+      <Section
+        title={task ? task.content : "Nie znaleziono zadania 😟"}
+        body={
+          <>
+            <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+          </>
+        }
+      />
+    </StyledContainer>
+  );
+}
+
+export default TaskPage;
