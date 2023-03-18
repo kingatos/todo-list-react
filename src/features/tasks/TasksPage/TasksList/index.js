e04483ddd1e5bbc130c3bc1c2b2058f29ea712db
Kinga Tos <toskinga@interia.pl>
2023-03-18 12:05:48 +0100

    formatted code

--- src/features/tasks/TasksPage/TasksList/index.js
+++ src/features/tasks/TasksPage/TasksList/index.js
@@ -1,43 +1,47 @@
-import { List, Item, Content, ToggleDoneButton, RemoveButton } from "./styled.js";
+import {
+  List,
+  Item,
+  Content,
+  ToggleDoneButton,
+  RemoveButton,
+  StyledLink,
+} from "./styled.js";
 import { useSelector, useDispatch } from "react-redux";
-import { toggleTaskDone, removeTasks, selectHideDone, selectTasksbyQuery } from "../.././tasksSlice";
-import { Link, useLocation } from "react-router-dom";
+import {
+  toggleTaskDone,
+  removeTasks,
+  selectHideDone,
+  selectTasksbyQuery,
+} from "../.././tasksSlice";
+import { useLocation } from "react-router-dom";
 import searchQueryParamName from "././../searchQueryParamName";
 
 const TasksList = () => {
-    const location = useLocation(); 
-    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
 
-    const tasks = useSelector(state => selectTasksbyQuery(state, query));
-    const hideDone = useSelector(selectHideDone);
-    
-    const dispatch = useDispatch();
+  const tasks = useSelector((state) => selectTasksbyQuery(state, query));
+  const hideDone = useSelector(selectHideDone);
 
-    return (
-        <List>
-        {tasks.map(task => (
-            <Item
-                hidden={task.done && hideDone}
-                key={task.id}
-            >
-                <ToggleDoneButton
-                    onClick={() => dispatch(toggleTaskDone(task.id))}
-                >
-                    {task.done ? "✔" : ""}
-                </ToggleDoneButton>
-                <Content
-                    done={task.done}
-                >
-                <Link to={`/zadania/${task.id}`}>{task.content} </Link>
-                </Content>
-                <RemoveButton
-                    onClick={() => dispatch(removeTasks(task.id))}
-                >
-                </RemoveButton>
-            </Item>
-        ))}
+  const dispatch = useDispatch();
+
+  return (
+    <List>
+      {tasks.map((task) => (
+        <Item hidden={task.done && hideDone} key={task.id}>
+          <ToggleDoneButton onClick={() => dispatch(toggleTaskDone(task.id))}>
+            {task.done ? "✔" : ""}
+          </ToggleDoneButton>
+          <Content done={task.done}>
+              <StyledLink to={`/zadania/${task.id}`}>{task.content} </StyledLink>
+          </Content>
+          <RemoveButton
+            onClick={() => dispatch(removeTasks(task.id))}
+          ></RemoveButton>
+        </Item>
+      ))}
     </List>
-    );
+  );
 };
 
 export default TasksList;
