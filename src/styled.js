8f434b22ea35bdf2d9466cfd8f1216b262d3a9b7
Kinga Tos <toskinga@interia.pl>
2023-03-18 12:06:14 +0100

    added more function of style to styled.js

--- src/styled.js
+++ src/styled.js
@@ -1,18 +1,37 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const StyledList = styled.ul`
-  justify-content: center;
+  display: flex;
+  place-content: center;
   list-style-type: none;
-  background-color: ${({ theme }) => theme.color.white};
-  max-width: 870px;
-  padding: 20px;
+  background-color: ${({ theme }) => theme.color.teal};
+  width: 100%;
+  padding: 10px;
   margin: 0 auto;
-  text-align: center;
+  border-bottom: 1px solid #ddd;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+export const ItemLi = styled.li`
+  margin-left: 20px;
 `;
 
-export const ItemA = styled.a`
+export const ItemA = styled(Link)`
   background-color: transparent;
   text-decoration: none;
-  color: black;
   cursor: pointer;
+  color: ${({ theme }) => theme.color.white};
+  font-weight:bold;
+
+  &:hover {
+    filter: brightness(110%);
+  }
+
+  &:active {
+    filter: brightness(120%);
+  }
 `;
