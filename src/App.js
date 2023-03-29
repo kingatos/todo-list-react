f508fd07a3c10b6b3b8a31a22d44ab9f6b3a40e6
Kinga Tos <toskinga@interia.pl>
2023-03-29 14:06:04 +0200

    renamed Menu to Navigation and created a file routes.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import AuthorPage from "./features/author/AuthorPage";
 import TasksPage from "./features/tasks/TasksPage/index";
 import TaskPage from "./features/tasks/TaskPage";
-import Menu from "./common/Menu/index";
+import Navigation from "./common/Navigation/index";
+import { toTask, toAuthor, toTasks } from "./routes";
 
 const App = () => (
   <HashRouter>
     <nav>
-      <Menu />
+      <Navigation />
       <Switch>
-        <Route path="/zadania/:id">
+        <Route path={toTask()}>
           <TaskPage />
         </Route>
-        <Route path="/zadania">
+        <Route path={toTasks()}>
           <TasksPage />
         </Route>
-        <Route path="/autor">
+        <Route path={toAuthor()}>
           <AuthorPage />
         </Route>
-        <Route path="/">
-          <Redirect to="/zadania" />
+        <Route>
+          <Redirect to={toTasks()} />
         </Route>
       </Switch>
     </nav>
